f27b37c0e79211b01c5c0a363e84b451524cf673
Kirill <kirill.pyulzyu@gmail.com>
2023-04-24 13:06:01 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,12 +6,12 @@ import PackageDescription
 let package = Package(
     name: "Secp256k1Swift",
     products: [
-        .library(
-            name: "CSecp256k1",
-            targets: ["CSecp256k1"]),
-        .library(
-            name: "Secp256k1Swift",
-            targets: ["Secp256k1Swift"]),
+//         .library(
+//             name: "CSecp256k1",
+//             targets: ["CSecp256k1"]),
+//         .library(
+//             name: "Secp256k1Swift",
+//             targets: ["Secp256k1Swift"]),
         .library(
             name: "BIP32Swift",
             targets: ["BIP32Swift"]),
@@ -23,23 +23,23 @@ let package = Package(
         .package(url: "https://github.com/mathwallet/RIPEMDSwift.git", from: "0.0.1")
     ],
     targets: [
-        .target(
-            name: "CSecp256k1",
-            dependencies: []),
-        .target(
-            name: "Secp256k1Swift",
-            dependencies: ["CSecp256k1"]),
+//         .target(
+//             name: "CSecp256k1",
+//             dependencies: []),
+//         .target(
+//             name: "Secp256k1Swift",
+//             dependencies: ["CSecp256k1"]),
         .target(
             name: "BIP32Swift",
             dependencies: ["Secp256k1Swift", "BigInt", "CryptoSwift", "Base58Swift", "RIPEMDSwift"]),
-        .testTarget(
-            name: "CSecp256k1Tests",
-            dependencies: ["CSecp256k1"]),
-        .testTarget(
-            name: "Secp256k1SwiftTests",
-            dependencies: ["Secp256k1Swift"]),
-        .testTarget(
-            name: "BIP32SwiftTests",
-            dependencies: ["Secp256k1Swift","BIP32Swift"]),
+//         .testTarget(
+//             name: "CSecp256k1Tests",
+//             dependencies: ["CSecp256k1"]),
+//         .testTarget(
+//             name: "Secp256k1SwiftTests",
+//             dependencies: ["Secp256k1Swift"]),
+//         .testTarget(
+//             name: "BIP32SwiftTests",
+//             dependencies: ["Secp256k1Swift","BIP32Swift"]),
     ]
 )
